c3d66862aa2de96a8a4f2f38fcacbc7900715cdb
codingdrizzle <codingfrancis100@gmail.com>
2023-08-19 14:22:15 +0000

    changes to error messages

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -1,24 +1,24 @@
 const bcrypt = require('bcrypt')
-const {findUser} = require('../user/services');
+const { findUser } = require('../user/services');
 const ApiResponses = require("../../utilities/api-responses");
-const {generateToken} = require("../../utilities/token-actions");
+const { generateToken } = require("../../utilities/token-actions");
 
 const login = async (req, res, next) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
-        const user = await findUser({email});
-        if (!user) return ApiResponses.notFoundResponse(res, 'Not Found', 'User account not found');
+        const user = await findUser({ email });
+        if (!user) return ApiResponses.notFoundResponse(res, 'User not found', 'User account not found');
 
-        if (!password) return ApiResponses.errorResponse(res, 'Cannot process request due to undefined password', 'No password provided');
+        if (!password) return ApiResponses.errorResponse(res, 'No password provided', 'Cannot process request due to undefined password');
 
         const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (isPasswordMatch && user) return res.status(200).json({message: 'Login Successful', token:generateToken({user})});
-        else return ApiResponses.errorResponse(res, 'Login not successful', 'Email or password incorrect');
+        if (isPasswordMatch && user) return res.status(200).json({ message: 'Login Successful', token: generateToken({ user }) });
+        else return ApiResponses.errorResponse(res,'Email or password incorrect', 'Login not successful' );
 
     } catch (error) {
         return next(error);
     }
 }
 
-module.exports = {login};
+module.exports = { login };
